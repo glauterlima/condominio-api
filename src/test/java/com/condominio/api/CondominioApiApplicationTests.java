5e0a09e413a6c2ed9f0ffcd288f1c8cab75f5c8d
Glauter Santos <glauterlimagmail.com>
2019-01-19 17:07:49 -0200

    Primeiro commmit

---
+++ src/test/java/com/condominio/api/CondominioApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.condominio.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CondominioApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
